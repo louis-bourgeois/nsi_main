b6c8c4b82c7c99294d30ec787f825b4da0126ed7
bourgeois.louis <bourgeois.louis@l326-05.IRIS>
2024-12-12 16:46:26 +0100

    fekfe

---
+++ Séquence 3 -  Les données structurées/Chapitre 4 - Types construits/tp_film_serie_v1.py
@@ -0,0 +1,59 @@
+medias = {
+    "Inception": {
+        "type": "Film",
+        "realisateur": "Christopher Nolan",
+        "annee": 2010,
+        "genre": "Science-Fiction",
+        "acteurs": ["Leonardo DiCaprio", "Elliot Page", "Tom Hardy"]
+    },
+    "Breaking Bad": {
+        "type": "Série",
+        "realisateur": "Vince Gilligan",
+        "annee": [2008, 2013],
+        "genre": "Drame",
+        "acteurs": ["Bryan Cranston", "Aaron Paul", "Anna Gunn"]
+    },
+    "Star Wars: Episode III": {
+        "type": "Film",
+        "realisateur": "George Lucas",
+        "annee": 2005,
+        "genre": "Science-Fiction",
+        "acteurs": ["Ewan McGregor", "Natalie Portman", "Hayden Christensen"]
+    },
+    "Avengers: Endgame": {
+        "type": "Film",
+        "realisateur": "Anthony et Joe Russo",
+        "annee": 2019,
+        "genre": "Super-Héros",
+        "acteurs": ["Robert Downey Jr.", "Chris Evans", "Scarlett Johansson"]
+    },
+    "Django Unchained": {
+        "type": "Film",
+        "realisateur": "Quentin Tarantino",
+        "annee": 2012,
+        "genre": "Western",
+        "acteurs": ["Jamie Foxx", "Christoph Waltz", "Leonardo DiCaprio"]
+    },
+    "Game of Thrones": {
+        "type": "Série",
+        "realisateur": "David Benioff et D.B. Weiss",
+        "annees": [2011, 2019],
+        "genre": "Fantasy",
+        "acteurs": ["Emilia Clarke", "Kit Harington", "Peter Dinklage"]
+    }
+}
+
+
+def add_media(titre, type, realisateur, annee, genre, acteurs):
+    exist = False
+    for media in medias.keys():
+        if media == titre:
+            exist = True
+
+    medias[titre] = {
+        "type": ,
+        "realisateur": ,
+        "annee": ,
+        "genre": ,
+        "acteurs"
+    }
